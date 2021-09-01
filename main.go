be6cf0ff8aad968de3e4b1f85dec40e10c3b54d1
Adrien Delorme <azr@users.noreply.github.com>
2021-09-01 13:28:17 +0200

    lint

--- main.go
+++ main.go
@@ -12,16 +12,6 @@ import (
 	googlecomputeimport "github.com/hashicorp/packer-plugin-googlecompute/post-processor/googlecompute-import"
 )
 
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.2"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
-)
-
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterBuilder(plugin.DEFAULT_NAME, new(googlecompute.Builder))
