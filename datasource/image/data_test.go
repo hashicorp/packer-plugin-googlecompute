77c0aac4efd8e92629d9c310e8cc15d9b2c5eca1
Tanmay Jain <tanmay.jain@hashicorp.com>
2025-05-20 16:55:22 +0530

    Added Tests for Image DataSource

---
+++ datasource/image/data_test.go
@@ -0,0 +1,45 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package image
+
+import (
+	"testing"
+)
+
+func TestDatasourceConfigure_EmptyProjectID(t *testing.T) {
+	d := &Datasource{
+		config: Config{
+			Filters: "name=ubuntu",
+		},
+	}
+	err := d.Configure()
+	if err == nil {
+		t.Fatal("expected error when project_id is missing")
+	}
+}
+
+func TestDatasourceConfigure_EmptyFilters(t *testing.T) {
+	d := &Datasource{
+		config: Config{
+			ProjectID: "test-project",
+		},
+	}
+	err := d.Configure()
+	if err == nil {
+		t.Fatal("expected error when filters are missing")
+	}
+}
+
+func TestDatasourceConfigure_ValidConfig(t *testing.T) {
+	d := &Datasource{
+		config: Config{
+			ProjectID: "test-project",
+			Filters:   "name=ubuntu",
+		},
+	}
+	err := d.Configure()
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+}
