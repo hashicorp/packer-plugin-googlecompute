161f926fa10d5e5e8dae65b6e0d689659c8f1e64
Yihao Wang <yihao@canva.com>
2025-11-27 21:55:48 +1300

    Add unit tests for oslogin_ssh_key_expire_after parameter
    - Enhance DriverMock to capture expiration time parameter
    - Add TestStepImportOSLoginSSHKey_withExpirationTime to verify expiration time is calculated and passed correctly
    - Add TestStepImportOSLoginSSHKey_withoutExpirationTime to verify expiration time is nil when not set
    - Tests follow existing test patterns in step_import_os_login_ssh_key_test.go

--- lib/common/driver_mock.go
+++ lib/common/driver_mock.go
@@ -124,6 +124,10 @@ type DriverMock struct {
 	UploadToBucketData       io.Reader
 	UploadToBucketResult     string
 	UploadToBucketError      error
+
+	ImportOSLoginSSHKeyUser           string
+	ImportOSLoginSSHKeyKey            string
+	ImportOSLoginSSHKeyExpirationTime *int64
 }
 
 func (d *DriverMock) CreateImage(project string, imageSpec *compute.Image) (<-chan *Image, <-chan error) {
@@ -380,6 +384,9 @@ func (d *DriverMock) CreateOrResetWindowsPassword(instance, zone string, c *Wind
 }
 
 func (d *DriverMock) ImportOSLoginSSHKey(user, key string, expirationTimeUsec *int64) (*oslogin.LoginProfile, error) {
+	d.ImportOSLoginSSHKeyUser = user
+	d.ImportOSLoginSSHKeyKey = key
+	d.ImportOSLoginSSHKeyExpirationTime = expirationTimeUsec
 	account := oslogin.PosixAccount{Primary: true, Username: "testing_packer_io"}
 	profile := oslogin.LoginProfile{
 		PosixAccounts: []*oslogin.PosixAccount{&account},
